61cd52d543b093f412afe82f463102613a47e288
CHIR4G7 <93506828+CHIR4G7@users.noreply.github.com>
2022-09-18 13:26:44 +0530

    Update familyclass.h

--- familyclass.h
+++ familyclass.h
@@ -1,10 +1,12 @@
 #include <string>
 
+
+//CREATING A CLASS SUCH THAT IT CONTAINS NAME ANS AGE OF THE FAMILY MEMEBER AND POINTER SFOR THE NEXT AND PREVIOUS MEMBERS OF THE FAMILY.
 class Family
 {
     public :
     Family *prev;
-    string name;
+    string name;//FOR NAME OF THE MEMBER
     int age;
     Family *next;
 
